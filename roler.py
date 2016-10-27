6a5c92d8a43fe9dcd501f6e937da57bf7598a4d4
Nikola Divic <thedivic@gamecredits.net>
2016-10-27 12:26:31 +0200

    Enable copying of roles from a given directory

--- roler.py
+++ roler.py
@@ -1,12 +1,20 @@
 import click
-from os import listdir, makedirs
+from os import listdir, makedirs, environ
 from os.path import isdir, join, exists, abspath
 from click import ClickException
+from shutil import copytree
 
 
-def create_at_path(path):
+def create_at_path(name, path, my):
     if exists(path):
         raise ClickException("Role %s already exists" % path)
+
+    roledir = environ.get("ROLER_ROLEDIR")
+
+    if my and roledir and exists(join(roledir, name)):
+        copytree(join(roledir, name), path)
+        print("Copied role: %s" % abspath(path))
+        return
 
     # Create the directory layout
     makedirs(join(path, 'defaults'))
@@ -26,16 +34,18 @@ def create_at_path(path):
 
 @click.command()
 @click.argument('name')
-def create_role(name):
+@click.option('--my/--no-my', default=False,
+              help='If ROLER_ROLEDIR envar is set it will copy the role from that directory')
+def create_role(name, my):
     """ A simple tool that creates an ansible role with the
         reccomended directory layout """
     roles_dir = [member for member in listdir('.')
                  if isdir(member) and member == 'roles']
 
     if roles_dir:
-        create_at_path(join(".", "roles", name))
+        create_at_path(name, join(".", "roles", name), my)
     else:
-        create_at_path(join(".", name))
+        create_at_path(name, join(".", name), my)
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='roler',
-    version='0.1',
+    version='0.2',
     description='A simple tool that creates an ansible role \
                  with the reccomended directory layout',
     author='Nikola Divic',
     author_email='divicnikola@gmail.com',
     url='https://github.com/TheDivic/roler',
-    download_url='https://github.com/TheDivic/roler/tarball/0.1',
+    download_url='https://github.com/TheDivic/roler/tarball/0.2',
     keywords=['ansible', 'role'],
     py_modules=['roler'],
     install_requires=[
